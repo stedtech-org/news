e5af457fb725acab89bda24020a601c99ae8801a
Atsu1209 <atsu1209.game@gmail.com>
2026-03-08 14:37:26 +0900

    Update news

---
+++ content.js
@@ -0,0 +1,15 @@
+const newsData = [
+    { title: "StedTechOrganization™ 3rd Anniversary.", date: "2026/03/04", link: "item/2026/2/index.html", image: "img/item/sted3rd.png" },
+    { title: "StedTech Pen Spinning Division 発足", date: "2026/02/27", link: "item/2026/1/index.html", image: "img/item/pendiv.png" },
+    { title: " StedTechStandard'STST'販売開始", date: "2025/11/01", link: "item/2025/2/index.html", image: "img/item/STST.png" },
+    { title: "StedTech-PublicRelations div.(広報部)が公式X(旧Twitter)を公開", date: "2025/01/07", link: "item/2025/1/index.html", image: "img/item/stedx2.png" },
+    { title: "Qiita内のOrganization月間ランキングにて6位を獲得", date: "2024/12/03", link: "item/2024/2/index.html", image: "img/item/stednew.png" },
+    { title: "StedTechホームページを開設", date: "2024", link: "item/2024/1/index.html", image: "img/item/stednew.png" }
+];
+
+
+
+
+
+
+
